dd430fd46519e01e2bdbc56d28c2cd1695e016c0
adm <adm@MacBook-Pro.local>
2017-05-22 20:22:26 +0300

    empty

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,22 @@
+ $(document).ready(function (){
+
+    $("#bg").fadeTo(1000, 0.4);
+    $(".form-wrap").fadeIn();
+ });
+
 $("#yes").click(function(){
     $(".form-container").hide();
+    $("#bg").css("position", "inherit");
+    $("#bg").css("opacity", "1");
 });
 
 $("#no").click(function(){
     window.open("http://www.google.com", "_self");
 });
 
-// $("body").click(function(){
-//     $("body").fadeTo(1000, 0.4);
+// $("button").click(function(){
+//     $("p:first").addClass("");
 // });
-
-$("button").click(function(){
-    $("p:first").addClass("");
-});
 
 // $("button").click(function(){
 //     $("p:first").removeClass("");
