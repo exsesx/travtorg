bd0da0366716f124c8d5c43ca011b6b98cd83b9a
adm <adm@MacBook-Pro.local>
2017-05-22 19:32:04 +0300

    ls

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+$("#yes").click(function(){
+    $(".form-container").hide();
+});
+
+$("#no").click(function(){
+    window.open("http://www.google.com", "_self");
+});
+
+// $("body").click(function(){
+//     $("body").fadeTo(1000, 0.4);
+// });
+
+$("button").click(function(){
+    $("p:first").addClass("");
+});
+
+// $("button").click(function(){
+//     $("p:first").removeClass("");
+// });
