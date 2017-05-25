d52641e9a542610d26c04da0354a2bf0f4bfe9cf
exsesx <exsesx@MacBook-Pro.local>
2017-05-26 00:27:00 +0300

    almost done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,6 +21,28 @@ function getCookie(cname) {
     return "";
 }
 
+function Utils() {
+
+}
+
+Utils.prototype = {
+    constructor: Utils,
+    isElementInView: function (element, fullyInView) {
+        var pageTop = $(window).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).height();
+
+        if (fullyInView === true) {
+            return ((pageTop < elementTop) && (pageBottom > elementBottom));
+        } else {
+            return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+        }
+    }
+};
+
+var Utils = new Utils();
+
 $(document).ready(function (){
     console.log('wtf');
      $('#collections-owl').owlCarousel({
@@ -30,21 +52,17 @@ $(document).ready(function (){
          itemsDesktopSmall : [979,3],
          nav:true,
          navText : ["<img src='images/arrows/left-arrow.png'>","<img src='images/arrows/right-arrow.png'>"]
-
      });
-
     $('#vendors-owl').owlCarousel({
-         items : 6,
+         items : 5,
          loop: false,
          itemsDesktop : [1199,3],
          itemsDesktopSmall : [979,3],
+         rewind: true,
          nav:true,
          navText : ["<img src='images/arrows/left-arrow.png'>","<img src='images/arrows/right-arrow.png'>"]
 
           });
-
-
-
      if(getCookie('popup') !== 'true') {
         $("#bg").css('position', 'fixed').css('pointer-events', 'none');
         $('.form-container').css('display', 'flex');
@@ -52,9 +70,34 @@ $(document).ready(function (){
     else {
             $("#bg").hide().css('pointer-events', 'auto');
         }
-
-
  });
+
+
+$(document).keyup(function (event) {    
+    var isCollectionsInView = Utils.isElementInView($('#collections-owl'), true);
+    var isVendorsInView = Utils.isElementInView($('#vendors-owl'), true);
+
+// 37 - left  39 - right
+
+    if (event.keyCode == 37 && isCollectionsInView) 
+    {
+       $('#collections-owl .owl-prev').trigger('click');
+    } 
+
+    else if (event.keyCode == 39 && isCollectionsInView) {
+       $('#collections-owl .owl-next').trigger('click');
+    }
+
+        if (event.keyCode == 37 && isVendorsInView) 
+    {
+       $('#vendors-owl .owl-prev').trigger('click');
+    } 
+
+    else if (event.keyCode == 39 && isVendorsInView) {
+       $('#vendors-owl .owl-next').trigger('click');
+    }
+
+});
 
 $("#yes").click(function(){
     $(".form-container").hide();
